d8c7e37a84f42dd848004591942381c596de37c4
Alex H <alex_lt@Alexs-MBP.fios-router.home>
2023-02-24 00:58:15 -0500

    completed initial projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,28 +4,56 @@ import { motion } from "framer-motion";
 type Props = {};
 
 const Projects = (props: Props) => {
-    const projects = [1, 2, 3, 4, 5];
+    const projects = [1, 2, 3];
     return (
-        <div className="h-screen relative flex flex-col overflow-hidden text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            className="h-screen relative flex flex-col overflow-hidden text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
             <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
                 Projects
             </h3>
             <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
                 {projects.map((project, i) => (
                     <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-                        <img
+                        <motion.img
+                            initial={{ y: -300, opacity: 0 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 1.2 }}
+                            viewport={{ once: true }}
                             src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80"
                             alt=""
+                            className="w-[350px] h-[350px]"
                         />
-                        <div>
-                            <h4>Case Study {i+1} of {projects.length}: UPS Clone</h4>
+                        <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+                            <h4 className="text-4xl font-semibold text-center">
+                                <span className="underline decoration-[#F7AB0A]/50">
+                                    Case Study {i + 1} of {projects.length}:
+                                </span>{" "}
+                                UPS Clone
+                            </h4>
+                            <p className="text-lg text-center md:text-left">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.
+                                Vestibulum lectus mauris ultrices eros. Aenean
+                                et tortor at risus. Morbi enim nunc faucibus a
+                                pellentesque sit amet porttitor eget. Enim
+                                lobortis scelerisque fermentum dui faucibus in
+                                ornare quam viverra. Suscipit tellus mauris a
+                                diam. Erat velit scelerisque in dictum. Leo vel
+                                fringilla est ullamcorper eget. Enim neque
+                                volutpat ac tincidunt vitae. Venenatis cras sed
+                                felis eget.
+                            </p>
                         </div>
                     </div>
                 ))}
                 {/* <Project /> */}
             </div>
             <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12"></div>
-        </div>
+        </motion.div>
     );
 };
 
