5e1d1895acedff4f1095a1eba475f9413c64c34a
Alex H <alex_lt@Alexs-MBP.fios-router.home>
2023-02-25 19:07:56 -0500

    removed the comment for images property from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'images.unsplash.com',
-  //     },
-  //   ],
-  // }
-}
+    reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
